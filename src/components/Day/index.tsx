e884869b187ca931637dadcbe52db78af63b472d
Martín <martincalderon92@outlook.com>
2023-05-08 17:00:47 -0300

    Receive and show picture of the day

--- src/components/Day/index.tsx
+++ src/components/Day/index.tsx
@@ -1,15 +1,29 @@
 import { useDate } from "@/contexts/DateContext"
+import Image from "next/image"
 
 interface DayProps {
   day: number
+  pictureUrl: string
 }
 
-const Day = ({ day }: DayProps) => {
+const Day = ({ day, pictureUrl }: DayProps) => {
   const { currentMonth } = useDate()
   const firstDay = currentMonth.startOf("month").day()
   const gridColumnStart = day === 1 ? firstDay : undefined
 
-  return <li style={{ gridColumnStart }}>{day}</li>
+  return (
+    <li style={{ gridColumnStart }}>
+      <p>{day}</p>
+      {pictureUrl && (
+        <Image
+          src={pictureUrl}
+          alt="Picture of the day"
+          width={100}
+          height={100}
+        />
+      )}
+    </li>
+  )
 }
 
 export default Day
