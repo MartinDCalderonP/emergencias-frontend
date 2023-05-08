3fb5a94ce1be1b8cef9a8b61ab350b61be1b6e3b
Martín <martincalderon92@outlook.com>
2023-05-08 14:32:22 -0300

    Create DateContext

---
+++ src/contexts/DateContext.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { createContext, ReactNode, useContext, useState } from "react"
+import dayjs from "dayjs"
+import "dayjs/locale/es"
+
+dayjs.locale("es")
+
+export const DateContext = createContext({
+  currentMonth: dayjs(),
+  setCurrentMonth: (date: dayjs.Dayjs) => {}
+})
+
+export const DateProvider = ({ children }: { children: ReactNode }) => {
+  const [currentMonth, setCurrentMonth] = useState(dayjs())
+
+  return (
+    <DateContext.Provider value={{ currentMonth, setCurrentMonth }}>
+      {children}
+    </DateContext.Provider>
+  )
+}
+
+export const useDate = () => {
+  const { currentMonth, setCurrentMonth } = useContext(DateContext)
+
+  return { currentMonth, setCurrentMonth }
+}
