5169abae5bc37727f43021f87a2bbc626978cf57
Martín <martincalderon92@outlook.com>
2023-05-08 16:24:41 -0300

    Add react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+import { QueryClient, QueryClientProvider } from "react-query"
 import { DateProvider } from "@/contexts/DateContext"
 import Title from "@/components/Title"
 import Calendar from "@/components/Calendar"
 
 const Home = () => {
   return (
-    <DateProvider>
-      <main>
-        <Title />
-        <Calendar />
-      </main>
-    </DateProvider>
+    <QueryClientProvider client={new QueryClient()}>
+      <DateProvider>
+        <main>
+          <Title />
+          <Calendar />
+        </main>
+      </DateProvider>
+    </QueryClientProvider>
   )
 }
 
