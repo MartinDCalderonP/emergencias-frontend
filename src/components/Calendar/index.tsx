00d7ac6ed4cca4a239570034197a771f28f16e7c
Martín <martincalderon92@outlook.com>
2023-05-08 17:00:36 -0300

    Use getPicturesOfTheMonth function in calendar component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,7 @@
 import styles from "./styles.module.css"
 import { useDate } from "@/contexts/DateContext"
+import { useQuery } from "react-query"
+import { getPicturesOfTheMonth } from "@/utils/utils"
 import CalendarHeader from "../CalendarHeader"
 import Day from "../Day"
 
@@ -11,11 +13,19 @@ const Calendar = () => {
     .fill(0)
     .map((_, i) => i + 1)
 
+  const { data, isLoading, error } = useQuery(["pictures"], () =>
+    getPicturesOfTheMonth(currentMonth)
+  )
+
+  if (isLoading) return <p>Loading...</p>
+
+  if (error) return <p>Something went wrong...</p>
+
   return (
     <ol className={styles.calendar}>
       <CalendarHeader />
       {monthDaysArray.map((day) => (
-        <Day key={day} day={day} />
+        <Day key={day} day={day} pictureUrl={data[day - 1]?.url} />
       ))}
     </ol>
   )
