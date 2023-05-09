b429fc58f75d3c3b60a22865081455d6e582adf5
Martín <martincalderon92@outlook.com>
2023-05-09 11:16:05 -0300

    Create basic modal

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,25 @@
+import { createPortal } from "react-dom"
+import { useRef } from "react"
+import useOnClickOutside from "@/hooks/useOnClickOutside"
+import styles from "./styles.module.css"
+
+interface ModalProps {
+  toggleModal: () => void
+}
+
+const Modal = ({ toggleModal }: ModalProps) => {
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  useOnClickOutside(modalRef, toggleModal)
+
+  return createPortal(
+    <div className={styles.overlay}>
+      <div className={`${styles.modal} ${styles.appearModal}`} ref={modalRef}>
+        Modal
+      </div>
+    </div>,
+    document.getElementById("modal-root") as HTMLElement
+  )
+}
+
+export default Modal
