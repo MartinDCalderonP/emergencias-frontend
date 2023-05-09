3a0b329f91096c2ed7b7a5beab194f35107e76c0
Martín <martincalderon92@outlook.com>
2023-05-09 11:09:40 -0300

    Add modal-root to layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout ({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <div id="modal-root" />
     </html>
   )
 }
