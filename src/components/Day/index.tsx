941f160900efd2f6925009253e43de7da4851fe6
Martín <martincalderon92@outlook.com>
2023-05-09 11:32:33 -0300

    Add logic to open modal

--- src/components/Day/index.tsx
+++ src/components/Day/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import styles from "./styles.module.css"
+import Modal from "../Modal"
 interface DayProps {
   day: number
   firstDayOfTheMonth: number
@@ -9,10 +11,23 @@ const Day = ({ day, firstDayOfTheMonth, pictureUrl }: DayProps) => {
   const gridColumnStart = day === 1 ? firstDayOfTheMonth : undefined
   const backgroundImage = pictureUrl ? `url(${pictureUrl})` : undefined
 
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleToggleModal = () => {
+    setIsModalOpen((prevState) => !prevState)
+  }
+
   return (
-    <li className={styles.day} style={{ gridColumnStart, backgroundImage }}>
-      {day}
-    </li>
+    <>
+      <li
+        className={styles.day}
+        onClick={handleToggleModal}
+        style={{ gridColumnStart, backgroundImage }}
+      >
+        {day}
+      </li>
+      {isModalOpen && <Modal toggleModal={handleToggleModal} />}
+    </>
   )
 }
 
