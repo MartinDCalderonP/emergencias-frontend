7d91a5be056135b442ec4f2aeb74ae00ead86feb
Martín <martincalderon92@outlook.com>
2023-05-09 16:47:30 -0300

    Capitalize title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,9 +1,13 @@
+import styles from "./styles.module.css"
 import { useDate } from "@/contexts/DateContext"
+import { capitalizeFirstLetter } from "@/utils/utils"
 
 const Title = () => {
   const { currentMonth } = useDate()
+  const title = currentMonth.format("MMMM YYYY")
+  const capitalizedTitle = capitalizeFirstLetter(title)
 
-  return <h1>{currentMonth.format("MMMM YYYY")}</h1>
+  return <h1 className={styles.title}>{capitalizedTitle}</h1>
 }
 
 export default Title
