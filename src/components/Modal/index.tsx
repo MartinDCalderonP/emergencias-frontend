413dcd13e54fed845256969a5c924c9fb6b3df78
Martín <martincalderon92@outlook.com>
2023-05-09 11:54:11 -0300

    Add optional to children in modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.module.css"
 import CloseIcon from "../CloseIcon"
 
 export interface ModalProps {
-  children: ReactNode
+  children?: ReactNode
   toggleModal: () => void
 }
 
