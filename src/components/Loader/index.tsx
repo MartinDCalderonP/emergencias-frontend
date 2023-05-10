efc71041e08b0b78ef11506c6e2ed7e4935da3fd
Martín <martincalderon92@outlook.com>
2023-05-10 08:57:53 -0300

    Add container to loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 import styles from "./styles.module.css"
 
-const Loader = () => <div className={styles.loader} data-testid="loader" />
+const Loader = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.loader} data-testid="loader" />
+    </div>
+  )
+}
 
 export default Loader
